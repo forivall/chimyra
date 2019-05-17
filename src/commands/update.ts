4e6e5752515dd6c6ded3e88d4560a11aa3c7dc06
Emily Marigold Klassen <forivall@gmail.com>
2019-05-17 10:58:34 -0700

    feat(update): add --dirty option which will pack a dep and install it

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -6,34 +6,68 @@ import {Argv} from 'yargs'
 import Command, {GlobalOptions} from '../command'
 import {NoCurrentPackage} from '../errors/validation'
 import {getBuildFile} from '../helpers/build-paths'
+import * as childProcess from '../helpers/child-process'
 import {minimize} from '../helpers/homedir'
 import ResolveTransitiveDependencies from '../helpers/resolve-transitive-deps'
-import {tuple} from '../helpers/types'
 import Package from '../model/package'
+import PackCommand from './pack';
+import getExecOpts from '../helpers/npm-exec-opts';
 
 export const command = 'update [deps..]'
 export const describe = 'Bump the version of dependencies changed since last release'
 
 // tslint:disable-next-line: typedef
 export function builder(y: Argv) {
-  return y
+  return y.options({
+    dirty: {
+      type: 'boolean',
+      default: false,
+      description: 'allow updating when directory is dirty'
+    }
+  })
 }
 
 export interface Options extends GlobalOptions {
   deps?: string[]
+  dirty?: boolean
+  install?: boolean
 }
 
+type DepMani = readonly [string, {
+  readonly name: string;
+  readonly version: string;
+}]
 export default class UpdateCommand extends ResolveTransitiveDependencies(Command) {
   options!: Options
   modifiedPackages!: Package[]
-  initialize(): void {
-    if (!this.currentPackage || !this.currentPackageNode) {
-      throw new NoCurrentPackage()
+  deps!: string[]
+  async initialize() {
+    if (!this.hasCurrentPackage()) throw new NoCurrentPackage()
+
+    this.deps = this.options.deps || [...this.currentPackageNode.localDependencies.keys()]
+
+    this.modifiedPackages = []
+
+    if (!this.options.dirty) {
+      this.cleanUpdate()
+      return
     }
 
-    const deps = this.options.deps || [...this.currentPackageNode.localDependencies.keys()]
+    if (this.deps.length !== 1) {
+      console.log('only one for now, plz')
+      return
+    }
 
-    const depManifests = deps.map((depId) => {
+    const {[0]: dep} = this.deps
+
+    const depNode = this.packageGraph.get(dep)
+
+    if (!depNode) throw new Error(`${dep} not found`)
+  }
+  cleanUpdate() {
+    if (!this.hasCurrentPackage()) throw new NoCurrentPackage()
+
+    const depManifests = this.deps.map((depId) => {
       const [name, requestedVersion = '*'] = depId.split('@')
       const version = requestedVersion === '*'
         ? this.packageGraph.get(name)!.version
@@ -41,10 +75,8 @@ export default class UpdateCommand extends ResolveTransitiveDependencies(Command
       return [depId, {name, version}] as const
     })
 
-    this.modifiedPackages = []
-
     const cpn = this.currentPackageNode
-    console.log('thqis.packageGraph:', [...this.packageGraph.keys()].join(' '))
+    console.log('this.packageGraph:', [...this.packageGraph.keys()].join(' '))
     console.log('cpn.externalDependencies:', [...cpn.externalDependencies.keys()].join(' '))
     console.log('cpn.localDependencies:', [...cpn.localDependencies.keys()].join(' '))
     const transDeps = this.resolveTransitiveDependencies()
@@ -74,7 +106,35 @@ export default class UpdateCommand extends ResolveTransitiveDependencies(Command
       }
     }
   }
+
   async execute(): Promise<void> {
+    if (this.options.dirty) {
+      if (!this.hasCurrentPackage()) throw new NoCurrentPackage()
+
+      const depNode = this.packageGraph.get(this.deps[0])!
+
+      const packFile = await Promise.resolve(new PackCommand({
+        ...this._args,
+        cwd: depNode.location,
+        dirty: true,
+      }, {
+        currentPackageNode: depNode
+      }))
+
+      const pkg = this.currentPackage
+
+      const packFileRel = path.relative(pkg.location, packFile)
+      this.logger.info('UPDATE', `Installing ${packFileRel}`)
+
+      await childProcess.spawnStreaming(
+        'npm',
+        ['install', packFileRel],
+        getExecOpts(pkg),
+        'UPDATE',
+      )
+      return
+    }
+
     if (!this.modifiedPackages || this.modifiedPackages.length === 0) {
       console.log('Nothing to do')
       return
